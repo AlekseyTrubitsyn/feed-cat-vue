3d5ac2ce58bd0a38311cd4294f34e8a884411983
Aleksey Trubtisyn <at.8441451@yandex.ru>
2018-02-03 14:36:56 +0300

    create base structure and styles

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1 class="page-header">Ты сегодня покормил кота?</h1>
+  </header>
+
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style>
+.page-header {
+  font-family: "Exo 2", sans-serif;
+  font-size: 36px;
+  font-weight: 100;
+  font-style: normal;
+
+  color: white;
+
+  padding: 42px 20px 24px;
+  text-align: center;
+}
+</style>
